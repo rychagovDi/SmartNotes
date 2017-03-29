74ae0f61dab4862e4b702ea5914114b195f506e5
Rychagov D.I <ruchagov-diman@mail.ru>
2017-03-29 13:05:35 +0400

    Initial commit

---
+++ SmartNotes/app/src/main/java/ru/rychagov/smartnotes/NotesListActivity.java
@@ -0,0 +1,13 @@
+package ru.rychagov.smartnotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotesListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.a_notes_list);
+	}
+}
