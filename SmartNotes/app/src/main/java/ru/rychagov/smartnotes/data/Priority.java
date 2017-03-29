479d8c985ebee98c2a940c898aef5875e9a0c6e9
Rychagov D.I <ruchagov-diman@mail.ru>
2017-03-30 00:23:46 +0400

    Добавил числовое значение для Priority

--- SmartNotes/app/src/main/java/ru/rychagov/smartnotes/data/Priority.java
+++ SmartNotes/app/src/main/java/ru/rychagov/smartnotes/data/Priority.java
@@ -1,8 +1,31 @@
 package ru.rychagov.smartnotes.data;
 
 public enum Priority {
-	NONE,
-	LOW,
-	MEDIUM,
-	HIGH
+	NONE(0),
+	LOW(1),
+	MEDIUM(2),
+	HIGH(3);
+
+	private int value;
+
+	Priority(int priority) {
+		value = priority;
+	}
+
+	public static Priority fromInt(int priority) {
+
+		for (Priority p : values()) {
+			if (p.getInt() == priority) {
+				return p;
+			}
+		}
+
+		return NONE;
+	}
+
+	public int getInt() {
+		return value;
+	}
 }
+
+
