7f1099f617ba7cb48c7669b712f2ed7e9bcaf909
Rychagov D.I <ruchagov-diman@mail.ru>
2017-03-29 14:47:12 +0400

    Небольшие изменения

--- SmartNotes/app/src/main/java/ru/rychagov/smartnotes/data/Note.java
+++ SmartNotes/app/src/main/java/ru/rychagov/smartnotes/data/Note.java
@@ -1,13 +1,21 @@
 package ru.rychagov.smartnotes.data;
 
+import android.util.Log;
+
 public class Note {
+
+	private static final String TAG = "Note";
 
 	private String title;
 	private String text;
+	private long time;
 
-	public Note(String title, String text) {
+	public Note(String title, String text, long time) {
 		this.title = title;
 		this.text = text;
+		this.time = time;
+
+		Log.i(TAG, String.format("Note was created. %s", this));
 	}
 
 	public String getTitle() {
@@ -16,5 +24,14 @@ public class Note {
 
 	public String getText() {
 		return text;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Title: %s. Timestamp: %s", title, time);
 	}
 }
