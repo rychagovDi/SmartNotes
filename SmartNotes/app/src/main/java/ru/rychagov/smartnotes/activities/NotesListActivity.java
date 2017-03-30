7a718a3b8e07250e593e19eb5494dda399bea322
Rychagov D.I <ruchagov-diman@mail.ru>
2017-03-31 00:39:14 +0400

    Добавил кнопку меню на ActionBar для добавления заметки

--- SmartNotes/app/src/main/java/ru/rychagov/smartnotes/activities/NotesListActivity.java
+++ SmartNotes/app/src/main/java/ru/rychagov/smartnotes/activities/NotesListActivity.java
@@ -1,11 +1,12 @@
 package ru.rychagov.smartnotes.activities;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import ru.rychagov.smartnotes.R;
 
-public class NotesListActivity extends Activity {
+public class NotesListActivity extends AppCompatActivity {
 
 	private static final String TAG = "NotesListActivity";
 
@@ -13,5 +14,11 @@ public class NotesListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_notes_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.notes_list_menu, menu);
+		return true;
 	}
 }
